eed109675027f12c1ebe62dc3bf6416b73654411
Gunpreet Singh <gunpreet.34@gmail.com>
2019-02-14 21:01:20 +0530

    Send mail

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ try{
 let app = express();
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","X-Requested-With");
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
     next();
 });
 app.use(bodyParser.urlencoded({extended:true}));
